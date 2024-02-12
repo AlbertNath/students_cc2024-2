93199b4ae34d426a3d9ecd7d64df0e865971e5ed
albert <natanael_pi@ciencias.unam.mx>
2024-02-12 15:12:21 -0600

    herencia/Hilos.java works

--- Practica1/src/main/java/kass/concurrente/herencia/Hilos.java
+++ Practica1/src/main/java/kass/concurrente/herencia/Hilos.java
@@ -6,16 +6,33 @@ package kass.concurrente.herencia;
  * @version 1.1
  */
 public class Hilos extends Thread {
-    public static final Integer HILOS = 3;
+    public static final Integer totalHilos = 3;
     public static final Integer TOTAL = 10000;
-    public static Integer contador = 0;
+    private static Integer contador = 0;
 
     @Override
     public void run() {
+        String s = Thread.currentThread().getName();
+        Integer id = Integer.parseInt(s.substring(s.length() - 1));
+        Integer sleepTime = (id == 0)? 1000 : 1;
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         for(int i = 0; i < TOTAL; i++)
             contador++;   
     }
     public static void main(String[] args) throws InterruptedException {
-        
+        Thread[] hilos = new Thread[totalHilos];
+        for (int i = 0; i < hilos.length; i++) {
+            hilos[i] = new Hilos();
+            hilos[i].start();
+        }
+
+        for (Thread i : hilos) 
+            i.join();
+
+        System.out.printf("Contador total: %d", contador);
     }
 }
