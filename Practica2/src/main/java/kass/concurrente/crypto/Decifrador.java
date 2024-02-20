9ed409405c1b1ee79448e571f42945929dc62d10
albert <natanael_pi@ciencias.unam.mx>
2024-02-19 23:01:02 -0600

    Decifrador.java docs

--- Practica2/src/main/java/kass/concurrente/crypto/Decifrador.java
+++ Practica2/src/main/java/kass/concurrente/crypto/Decifrador.java
@@ -2,17 +2,42 @@ package kass.concurrente.crypto;
 
 import kass.concurrente.constants.Constante;
 
+/**
+ * Clase que reoresenta una instancia para romper 
+ * contrseñas por fuerza bruta. Implementa la 
+ * interfaz <code>Runnable</code> dado que queremos
+ * utilizarla con hilos.
+ * @author PaoPatrol. 
+ * @version 1.0
+ */
 public class Decifrador implements Runnable {
+    /** La llave a romper */
     private String key;
+    /** El alfabeto del sistema criptográfico */
     private char[] alphabet;
+    /** Variable compartida para detener los hilos */
     private volatile Boolean found;
 
+    /**
+     * Constructor de la clase.
+     * @param key la llave a romper.
+     */
     public Decifrador(String key){
         this.key = key;
         this.alphabet = Constante.ALFABETO.toCharArray();
         this.found = false;
     }
 
+    /**
+     * Método recursivo privado para romper la llave. 
+     * Se realiza fuerza bruta sobre todas las posibles 
+     * combinaciones de letras del alfabeto. Fijamos la 
+     * primera letra de la potencial llave a probar y 
+     * agregamos letras del abecedario, probando con 
+     * las posibles combinaciones de manera recursiva.
+     * @param password la llave a probar
+     * @throws Exception 
+     */
     private void crack(String password) throws Exception {
 
         if(this.found)
