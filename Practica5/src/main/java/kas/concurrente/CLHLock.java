95ad0ad779821975c6619aabf89b357d5f01734e
albert <natanael_pi@ciencias.unam.mx>
2024-04-02 17:19:30 -0600

    other locks

--- Practica5/src/main/java/kas/concurrente/CLHLock.java
+++ Practica5/src/main/java/kas/concurrente/CLHLock.java
@@ -1,17 +1,41 @@
 package kas.concurrente;
 
+import java.util.concurrent.atomic.AtomicReference;
+import kas.concurrente.QNode;
+
 public class CLHLock implements Lock {
+    AtomicReference<QNode> tail;
+    ThreadLocal<QNode> myPred;
+    ThreadLocal<QNode> myNode;
+
+    public CLHLock() {
+        tail = new AtomicReference<>();
+        myNode = new ThreadLocal<QNode>() {
+            protected QNode initialValue() {
+                return new QNode();
+            }
+        };
+        myPred = new ThreadLocal<QNode>() {
+            protected QNode initialValue() {
+                return null;
+            }
+        };
+    }
 
     @Override
     public void lock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lock'");
+        QNode qnode = myNode.get();
+        qnode.set(true);
+        QNode pred = tail.getAndSet(qnode);
+        myPred.set(pred);
+        while (pred.get());
     }
 
     @Override
     public void unlock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'unlock'");
+        QNode qnode = myNode.get();
+        qnode.set(false);
+        myNode.set(myPred.get());
     }
     
 }
