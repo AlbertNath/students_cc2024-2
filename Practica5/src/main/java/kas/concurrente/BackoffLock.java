3ec80114b65a463623d6a9ed1bd96f60e410b16c
albert <natanael_pi@ciencias.unam.mx>
2024-04-01 21:50:09 -0600

    Some locks

--- Practica5/src/main/java/kas/concurrente/BackoffLock.java
+++ Practica5/src/main/java/kas/concurrente/BackoffLock.java
@@ -1,17 +1,30 @@
 package kas.concurrente;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class BackoffLock implements Lock {
+    private AtomicBoolean state = new AtomicBoolean(false);
+    private static final int MIN_DELAY = 0;
+    private static final int MAX_DELAY = 1000;
 
     @Override
     public void lock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lock'");
+        // backoff??
+        while (true) {
+            while (state.get());
+            if (!state.getAndSet(true)){
+                return;
+            } else {
+                //backoff
+            }
+
+        }
     }
 
     @Override
     public void unlock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'unlock'");
+        this.state.set(false);
     }
     
 }
