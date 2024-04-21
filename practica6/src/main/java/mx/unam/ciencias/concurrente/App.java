05494e2d4ac2c1e068564eeb3b9ff21712b1f01f
albert <natanael_pi@ciencias.unam.mx>
2024-04-21 13:30:56 -0600

    practica6 init

---
+++ practica6/src/main/java/mx/unam/ciencias/concurrente/App.java
@@ -0,0 +1,13 @@
+package mx.unam.ciencias.concurrente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
