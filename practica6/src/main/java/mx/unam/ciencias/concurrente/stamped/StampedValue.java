10057055673b1ca6bd65c598ca98a1e71aa5d02e
Paolatae2505 <75380831+Paolatae2505@users.noreply.github.com>
2024-04-24 21:29:56 -0400

    Add files via upload
    StampedValue

---
+++ practica6/src/main/java/mx/unam/ciencias/concurrente/stamped/StampedValue.java
@@ -0,0 +1,28 @@
+package main.java.mx.unam.ciencias.concurrente.stamped;
+
+public class StampedValue<T> {
+    public long stamp;
+    public T value;
+
+    // Valor inicial con marca de tiempo cero
+    public StampedValue(T init) {
+        this.stamp = 0;
+        this.value = init;
+    }
+
+    // Valores posteriores con marca de tiempo proporcionada
+    public StampedValue(long stamp, T value) {
+        this.stamp = stamp;
+        this.value = value;
+    }
+
+    public static StampedValue<?> max(StampedValue<?> x, StampedValue<?> y) {
+        if (x.stamp > y.stamp) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public static final StampedValue<?> MIN_VALUE = new StampedValue<>(null);
+}
